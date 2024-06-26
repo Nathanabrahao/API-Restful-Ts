b62aa1deb33409871d6a66118e040231cb1a0861
Nathanabrahao <nathanntc6@gmail.com>
2024-06-26 20:55:35 -0300

    Insert Ts in the project

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import config from "config"
+
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(3000, async () =>{
+    console.log("Server is running on port 3000")
+})
